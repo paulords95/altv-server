4e1df90543fb8ab6904402699e929e2fb02aa3b6
Paulo Silva <paulords95@gmail.com>
2020-03-22 15:49:26 -0300

    added new features

--- client/client.mjs
+++ client/client.mjs
@@ -2,34 +2,67 @@ import * as alt from 'alt';
 import * as native from 'natives'
 import * as chat from 'chat'
 
-let view = new alt.WebView("http://resource/client/html/index.html");
-let speedoShown = false;
-let playerVehicle = false;
+let view = new alt.WebView("http://resource/client/html/index.html")
+let speedoShown = false
+let playerVehicle = false
 
 alt.setInterval(() => {
-    if (!playerVehicle) return;
+    if (!playerVehicle) return
     if (speedoShown) {
         view.emit('drawSpeedo', playerVehicle.speed, playerVehicle.gear, playerVehicle.rpm)
     }
-}, 1);
+}, 1)
 
 
-native.unlockMinimapPosition()
+let blips = {}
+let myID = 0
 
+alt.onServer('myID', playerID => {
+    myID = playerID
+})
+
+alt.onServer('createPlayerBlip', (playerID, playerName, pos) => {
+    if (myID == playerID) {
+        return
+    }
+
+    if (blips[playerID] !== undefined) {
+        return
+    }
+
+    blips[playerID] = new alt.PointBlip(pos.x, pos.y, pos.z)
+    blips[playerID].sprite = 286
+    blips[playerID].color = 47
+    blips[playerID].name = playerName
+
+})
+
+alt.onServer('deletePlayerBlip', playerID => {
+    if (blips[playerID] !== undefined) {
+        blips[playerID].destroy()
+        delete blips[playerID]
+    }
+})
+
+alt.onServer('updatePlayerBlip', (playerID, pos) => {
+    if (blips[playerID] !== undefined) {
+        blips[playerID].pos = pos
+    }
+})
 
 view.on('speedoLoaded', () => {
-    speedoShown = true;
-});
+    speedoShown = true
+})
 
 view.on('speedoUnloaded', () => {
-    speedoShown = false;
+    speedoShown = false
 })
 
 alt.onServer("playerEnterVehicle", (vehicle, seat) => {
     playerVehicle = vehicle;
     if (seat == 1) { //driver
         if (!speedoShown) {
-            view.emit('showSpeedo', true);
+            view.emit('showSpeedo', true)
         }
     }
 })
@@ -38,7 +71,7 @@ alt.onServer("playerLeftVehicle", (seat) => {
     playerVehicle = false;
     if (seat == 1) { //driver
         if (speedoShown) {
-            view.emit('showSpeedo', false);
+            view.emit('showSpeedo', false)
         }
     }
 })
@@ -47,70 +80,61 @@ alt.onServer("playerChangedVehicleSeat", (vehicle, seat) => {
     playerVehicle = vehicle;
     if (seat == 1) { //driver
         if (!speedoShown) {
-            view.emit('showSpeedo', true);
+            view.emit('showSpeedo', true)
         }
     }
 })
 
 alt.onServer("enterCarro", (vehicle, name) => {
     let loaded = false
-    let hash = native.getHashKey(name);
+    let hash = native.getHashKey(name)
     let interval = null
     const localPlayer = alt.Player.local.scriptID;
-  interval = alt.setInterval(() => {
-        if(native.hasModelLoaded(hash)) {
+    interval = alt.setInterval(() => {
+        if (native.hasModelLoaded(hash)) {
             alt.clearInterval(interval)
 
             alt.nextTick(() => {
-                native.setPedIntoVehicle(localPlayer, vehicle.scriptID, -1);
-                let c1 = Math.floor(Math.random() * 158);
-                let c2 = Math.floor(Math.random() * 158);
-                native.setVehicleColours(vehicle.scriptID, c1, c2);
+                native.setPedIntoVehicle(localPlayer, vehicle.scriptID, -1)
             });
         }
-   }, 50)
-   
+    }, 50)
 })
 
-
-
-
-
-
-
-let menuOpen = false;
-let menu = null;
+let menuOpen = false
+let menu = null
 
 alt.on('keydown', (key) => {
 
 
     if (key == 114) {
         let player = native.playerId()
-        let playerPos = native.getOffsetFromEntityInWorldCoords(player, 0, 5, 2);
+        let playerPos = native.getOffsetFromEntityInWorldCoords(player, 0, 5, 2)
         alt.log(playerPos.x)
     }
 
     if (key == 115) {
         let player = native.playerId()
         let hash = native.getHashKey('a_m_y_acult_01')
-     
-        native.requestModel(hash);
+
+        native.requestModel(hash)
         if (native.hasModelLoaded(hash)) {
-            native.setPlayerModel(player, hash);
+            native.setPlayerModel(player, hash)
         }
     }
+
 
     if (key === 112) {
         if (!menuOpen) {
             alt.showCursor(true)
             // http://resource/client/html/menu.html
-            menu = new alt.WebView("https://google.com.br", true);
+            menu = new alt.WebView("https://google.com.br", true)
             menu.on('rmv', () => {
                 alt.emitServer('rmv')
             })
         } else {
             alt.showCursor(false)
-            menu.destroy();
+            menu.destroy()
         }
 
         menuOpen = !menuOpen
@@ -118,30 +142,48 @@ alt.on('keydown', (key) => {
 });
 
 alt.onServer('posResponse', pos => {
-    let hash = native.getHashKey('infernus');
-    native.requestModel(hash);
-    if(native.hasModelLoaded(hash)){
-        native.createVehicle(hash, pos.x, pos.y, pos.z, 0, true, false, false);
+    let hash = native.getHashKey('infernus')
+    native.requestModel(hash)
+    if (native.hasModelLoaded(hash)) {
+        native.createVehicle(hash, pos.x, pos.y, pos.z, 0, true, false, false)
     }
 })
 
 
 
 alt.onServer('player:position', position => {
-    alt.log(`Position=${position.x}, ${position.y}, ${position.z}`);
-    alt.log(`/tp ${position.x} ${position.y}`);
+    alt.log(`Position=${position.x}, ${position.y}, ${position.z}`)
+    alt.log(`/tp ${position.x} ${position.y}`)
 });
 
 
 
-const weather = {
-    ExtraSunny: 0,
-    Clear: 1,
-    Clouds: 2,
-    Overcast: 5,
-    Rain: 6,
-    Thunder: 7,
-    LightRain: 8,
-};
+alt.onServer('fix', vehicle => {
+    if (vehicle !== null) {
+        native.setVehicleFixed(vehicle.scriptID)
+    }
+})
 
-alt.setWeatherSyncActive(true);
+
+alt.onServer('obj', pos =>{
+    let obj = native.getHashKey('apa_prop_flag_brazil');
+    native.createObject(obj, pos.x, pos.y, pos.z, true, false, false);
+})
+
+
+alt.onServer('hora', hour => {
+    if (hour >= 24) {
+        hour = 23
+    }
+
+    if (hour <= 0) {
+        hour = 0
+    }
+
+    native.setClockTime(hour, 1, 1)
+})
+
+
+native.requestIpl('hei_carrier')
+
+native.requestIpl('ex_dt1_11_office_02b')
--- server/server.mjs
+++ server/server.mjs
@@ -1,113 +1,21 @@
 import * as alt from 'alt'
 import * as chat from 'chat'
-import peds  from '../client/peds.js'
+import peds from '../client/peds.js'
 import vehicles from '../client/vehicles.js'
+import weaponList from '../client/weapons.js'
 
 let globalCars = {}
+let globalPlayerPos = {}
 
 const positions = {
     'default': [-1062.4483642578125, -2985.73193359375, 13.17138671875],
     'pallets': [824.2417602539062, -809.3406372070312, 26.5164794921875],
     'praia_zancudo': [-2818.4833984375, 3603.6923828125, -0.4769287109375],
     'aeroporto': [-1062.4483642578125, -2985.73193359375, 13.17138671875],
-    'altoagua' : [4081.7802734375, -2030.3077392578125, 2431.27197265625],
-    'altocidade': [690.5538330078125, -1043.6043701171875, 2246.766357421875]
-}
-
-const weaponList = {
-    // melee
-    'unarmed': -1569615261,
-    'knife': -1716189206,
-    'nightstick': 1737195953,
-    'hammer': 1317494643,
-    'bat': -1786099057,
-    'crowbar': -2067956739,
-    'golfclub': 1141786504,
-    'bottle': -102323637,
-    'dagger': -1834847097,
-    'hatchet': -102973651,
-    'knuckleduster': -656458692,
-    'machete': -581044007,
-    'flashlight': -1951375401,
-    'switchblade': -538741184,
-    'poolcue': -1810795771,
-    'wrench': 419712736,
-    'battleaxe': -853065399,
-    // pistols
-    'pistol': 453432689,
-    'pistolmk2': 3219281620,
-    'combatpistol': 1593441988,
-    'pistol50': -1716589765,
-    'snspistol': -1076751822,
-    'heavypistol': -771403250,
-    'vintagepistol': 137902532,
-    'marksmanpistol': -598887786,
-    'revolver': -1045183535,
-    'appistol': 584646201,
-    'stungun': 911657153,
-    'flaregun': 1198879012,
-    // machine guns
-    'microsmg': 324215364,
-    'machinepistol': -619010992,
-    'smg': 736523883,
-    'smgmk2': 2024373456,
-    'assaultsmg': -270015777,
-    'combatpdw': 171789620,
-    'mg': -1660422300,
-    'combatmg': 2144741730,
-    'combatmgmk2': 3686625920,
-    'gusenberg': 1627465347,
-    'minismg': -1121678507,
-    // assault rifles
-    'assaultrifle': -1074790547,
-    'assaultriflemk2': 961495388,
-    'carbinerifle': -2084633992,
-    'carbineriflemk2': 4208062921,
-    'advancedrifle': -1357824103,
-    'specialcarbine': -1063057011,
-    'bullpuprifle': 2132975508,
-    'compactrifle': 1649403952,
-    // snipers
-    'sniperrifle': 100416529,
-    'heavysniper': 205991906,
-    'heavysnipermk2': 177293209,
-    'marksmanrifle': -952879014,
-    // shotguns
-    'pumpshotgun': 487013001,
-    'sawnoffshotgun': 2017895192,
-    'bullpupshotgun': -1654528753,
-    'assaultshotgun': -494615257,
-    'musket': -1466123874,
-    'heavyshotgun': 984333226,
-    'doublebarrelshotgun': -275439685,
-    'autoshotgun': 317205821,
-    // heavy weapons
-    'grenadelauncher': -1568386805,
-    'rpg': -1312131151,
-    'minigun': 1119849093,
-    'firework': 2138347493,
-    'railgun': 1834241177,
-    'hominglauncher': 1672152130,
-    'grenadelaunchersmoke': 1305664598,
-    'compactlauncher': 125959754,
-    // thrown
-    'grenade': -1813897027,
-    'widowmaker': 0xB62D1F67,
-    'stickybomb': 741814745,
-    'proximitymine': -1420407917,
-    'bzgas': -1600701090,
-    'molotov': 615608432,
-    'fireextinguisher': 101631238,
-    'petrolcan': 883325847,
-    'flare': 1233104067,
-    'ball': 600439132,
-    'snowball': 126349499,
-    'smokegrenade': -37975472,
-    'pipebomb': -1169823560,
-    // utility
-    'parachute': -72657034,
-    'carro': 2741846334,
-    'porrada': 2725352035,
+    'altoagua': [4081.7802734375, -2030.3077392578125, 2431.27197265625],
+    'altocidade': [-774.7780151367188, -1226.123046875, 2511.56103515625],
+    'portaaviao': [3084.73, -4770.709, 15.26167],
+    'mazebank' : [-75.8466, -826.9893, 243.3859],
 }
 
 alt.onClient('pos', (player, data) => {
@@ -119,7 +27,21 @@ chat.registerCmd('pos', (player, args) => {
     alt.emitClient(player, 'player:position', player.pos)
 })
 
+function registerPlayersBlips() {
+    for (let p of alt.Player.all) {
+        alt.emitClient(null, 'createPlayerBlip', p.id, p.name, p.pos)
+    }
+}
+
+function removePlayerBlips() {
+    for (let p of alt.Player.all) {
+        alt.emitClient(null, 'deletePlayerBlip', p.id)
+    }
+}
+
 alt.on('playerConnect', (player) => {
+    alt.emitClient(player, 'myID', player.id)
+
     globalCars[player.id] = []
     chat.broadcast(`${player.name} conectado`)
 
@@ -128,17 +50,35 @@ alt.on('playerConnect', (player) => {
     let ped = pedArr[Math.floor(Math.random() * arrLen)]
 
     player.model = ped
-    player.spawn(positions.default[0], positions.default[1], positions.default[2], 1000)
+
+    if (globalPlayerPos[player.socialId] !== undefined) {
+        player.spawn(globalPlayerPos[player.socialId].x, globalPlayerPos[player.socialId].y, globalPlayerPos[player.socialId].z, 1000)
+    } else {
+        player.spawn(positions.default[0], positions.default[1], positions.default[2], 1000)
+    }
+
+    registerPlayersBlips()
 })
+
+alt.setInterval(() => {
+    for (let p of alt.Player.all) {
+        globalPlayerPos[p.socialId] = p.pos
+        alt.emitClient(null, 'updatePlayerBlip', p.id, p.pos)
+    }
+}, 500)
 
 alt.on('playerDisconnect', (player, reason) => {
     chat.broadcast(`${player.name} desconectado`)
+    globalPlayerPos[player.socialId] = player.pos
+
     if (globalCars[player.id] !== undefined) {
         for (let v of globalCars[player.id]) {
             v.destroy()
         }
-        globalCars[player.id] = [];
+        globalCars[player.id] = []
     }
+
+    removePlayerBlips()
 })
 
 alt.on('playerDeath', (player, killer, weapon) => {
@@ -163,12 +103,11 @@ chat.registerCmd('arma', (player, args) => {
     }
 })
 
-chat.registerCmd('todasarmas',(player) => {
+chat.registerCmd('todasarmas', (player) => {
     for (let key in weaponList) {
         player.giveWeapon(weaponList[key], 1000, true)
     }
 });
-
 
 chat.registerCmd('veh', (player, args) => {
     if (args.length === 0) {
@@ -181,6 +120,10 @@ chat.registerCmd('veh', (player, args) => {
     try {
         chat.broadcast(`${player.name}: /veh ${args[0]}`)
         let v = new alt.Vehicle(args[0], player.pos.x + 3, player.pos.y, player.pos.z, 0, 0, 0)
+        let c1 = Math.floor(Math.random() * 158)
+        let c2 = Math.floor(Math.random() * 158)
+        v.primaryColor = c1
+        v.secondaryColor = c2
         globalCars[player.id].push(v)
         alt.emitClient(player, 'enterCarro', v, args[0])
     } catch (e) {
@@ -192,16 +135,18 @@ chat.registerCmd('veh', (player, args) => {
 chat.registerCmd('anyveh', (player) => {
     chat.broadcast(`${player.name}: /anyveh`)
 
-
     const vehArr = Object.keys(vehicles)
     let vehArrLen = vehArr.length
     let vehicle = vehArr[Math.floor(Math.random() * vehArrLen)]
     let v = new alt.Vehicle(vehicle, player.pos.x + 3, player.pos.y, player.pos.z, 0, 0, 0)
-        globalCars[player.id].push(v)
-        alt.emitClient(player, 'enterCarro', v, vehicle)
+    let c1 = Math.floor(Math.random() * 158)
+    let c2 = Math.floor(Math.random() * 158)
+    v.primaryColor = c1
+    v.secondaryColor = c2
+    globalCars[player.id].push(v)
+    chat.broadcast(`${player.name}: Carro ${vehicle}`)
+    alt.emitClient(player, 'enterCarro', v, vehicle)
 })
-
-
 
 chat.registerCmd('tp', (player, args) => {
     if (args.length === 0) {
@@ -245,10 +190,22 @@ chat.registerCmd('rmv', (player) => {
         for (let v of globalCars[player.id]) {
             v.destroy()
         }
-        globalCars[player.id] = [];
+        globalCars[player.id] = []
     }
 
     chat.send(player, `Carros removidos`)
+})
+
+alt.on('resourceStop', () => {
+    alt.log('Yes, stopped')
+
+    for (let playerID in globalCars) {
+        for (let v of globalCars[playerID]) {
+            v.destroy()
+        }
+
+        delete globalCars[playerID]
+    }
 })
 
 alt.onClient('rmv', (player) => {
@@ -266,7 +223,8 @@ chat.registerCmd('skin', (player, args) => {
     if (args.length === 0) {
         chat.send(player, `Você deve informar o nome da skin`)
         chat.send(player, `Exemplo:`)
-        chat.send(player, `/skin Business02AFM`)
+        chat.send(player, `/skin ig_abigail`)
+        chat.send(player, `Sua skin atual: ${player.model}`)
         return
     }
 
@@ -288,4 +246,63 @@ alt.on('playerLeftVehicle', (player, vehicle, seat) => {
 
 alt.on('playerChangedVehicleSeat', (player, vehicle, oldSeat, newSeat) => {
     alt.emitClient(player, 'playerChangedVehicleSeat', vehicle, newSeat)
+})
+
+chat.registerCmd('fix', (player) => {
+    alt.emitClient(player, 'fix', player.vehicle)
+})
+
+const weather = {
+    'sol': 0,
+    'claro': 1,
+    'nuvens': 2,
+    'poluicao': 3,
+    'neblina': 4,
+    'nublado': 5,
+    'chuva': 6,
+    'tempestade': 7,
+    'chuvafraca': 8,
+    'poluicaochuvafraca': 9,
+    'nevefraca': 10,
+    'nevevento': 11,
+    'neve': 12,
+};
+
+const weatherNames = Object.keys(weather)
+
+chat.registerCmd('tempo', (player, args) => {
+    if (args.length === 0) {
+        chat.send(player, `Você deve informar o nome do tempo`)
+        chat.send(player, `Exemplo:`)
+        chat.send(player, `/tempo sol`)
+        chat.send(player, `Valores: ${weatherNames.join(', ')}`)
+        return
+    }
+    
+    try {
+        for (let p of alt.Player.all) {
+            p.setWeather(weather[args[0]])
+        }
+        chat.broadcast(`${player.name}: /tempo ${args[0]}`)
+    } catch (e) {
+        chat.send(player, `Não foi possível encontrar o tempo`)
+    }
+})
+
+chat.registerCmd('hora', (player, args) => {
+    if (args.length === 0) {
+        chat.send(player, `Você deve informar a hora`)
+        chat.send(player, `Exemplo:`)
+        chat.send(player, `/hora 10`)
+        return
+    }
+    
+    chat.broadcast(`${player.name}: /hora ${args[0]}`)
+    alt.emitClient(null, 'hora', args[0])
+})
+
+
+
+chat.registerCmd('obj', (player) => {
+    alt.emitClient(null, 'obj', player.pos)
 })
